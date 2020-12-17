b83b3e2cc18566d0d31a051124539eb5c536202e
moulaygit <68529397+moulaygit@users.noreply.github.com>
2020-12-16 19:13:19 -0700

    Update App.java

--- src/main/java/org/moulay/javaalfred/App.java
+++ src/main/java/org/moulay/javaalfred/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this just a test for failures" ;
+        System.out.println( "Hello World! this just a test for failures") ;
     }
 }
